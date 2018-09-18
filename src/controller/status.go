1df30fbc5c074a62374142295b7f21fdc9e38000
Chengguang Yu <yuchengguang2014@gmail.com>
2018-09-18 14:59:30 -0400

    SC-0001 remove logs

--- src/controller/status.go
+++ src/controller/status.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/comodo/comodoca-status-api/common"
 	"net/http"
 )
@@ -14,35 +13,24 @@ func StatusHandler(w http.ResponseWriter, _ *http.Request) {
 		if ok {
 			status := *response
 			w.Header().Set("Content-Type", "application/json")
-			//statusJson, err := json.Marshal(status)
-			//if err != nil {
-			//panic(err)
-			//}
 			if status.Status == "available" {
 				w.WriteHeader(http.StatusOK)
 			} else {
-				w.WriteHeader(http.StatusOK)
+				w.WriteHeader(http.StatusInternalServerError)
 			}
 			json.NewEncoder(w).Encode(status)
 			common.LastStatus = status
-			//w.Write(statusJson)
 			close(common.StatusChannel)
 		} else {
-			fmt.Print("called")
 
 			w.Header().Set("Content-Type", "application/json")
-			//statusJson, err := json.Marshal(common.LastStatus)
 
-			//if err != nil {
-			//	panic(err)
-			//}
 			if common.LastStatus.Status == "available" {
 				w.WriteHeader(http.StatusOK)
 			} else {
-				w.WriteHeader(http.StatusOK)
+				w.WriteHeader(http.StatusInternalServerError)
 			}
 			json.NewEncoder(w).Encode(common.LastStatus)
-			//w.Write(statusJson)
 		}
 
 		return
